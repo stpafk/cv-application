c7288d8f7d9de0d66e70758e2159f214287622ef
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-06-28 17:19:10 +0000

    start general component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Resume from './components/Resume';
+
+class App extends Component {
+    render() {
+        return (
+            <Resume />
+        );
+    };
+};
+
+export default App;
---
+++ src/components/Resume.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import General from './General';
+
+class Resume extends Component {
+
+    render() {
+        return (
+            <div className="yourResume">
+                <General />
+            </div>
+        );
+    };
+
+};
+
+export default Resume;
