a87ad9152b4045638c4fa53a9f37c9c94cad9377
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-08-22 14:05:31 +0000

    del downlaod

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,8 +3,6 @@ import General from './General';
 import Contact from './Contact';
 import Experience from './Experience';
 import Education from './Education';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
 
 class Resume extends Component {
 
@@ -15,36 +13,6 @@ class Resume extends Component {
             isView: false,
             loader: false,
         }
-    }
-
-    downloadPDF = () => {
-        this.setState({
-            ...this.state,
-            loader: true
-        });
-
-        const capture = document.querySelector(".yourResume");
-        if (capture) {
-      
-            html2canvas(capture).then((canvas) => {
-      
-              const imgData = canvas.toDataURL('img/png');
-      
-              const doc = new jsPDF();
-      
-              const componentWidth = doc.internal.pageSize.getWidth();
-      
-              const componentHeight = doc.internal.pageSize.getHeight();
-      
-              doc.addImage(imgData, 'PNG', 0, 0, componentWidth, componentHeight);
-      
-              doc.save('certificate.pdf');
-      
-            });
-      
-          }
-      
-
     }
 
     toggleViewMode = (id) => {
@@ -70,11 +38,6 @@ class Resume extends Component {
             <div className="viewmode buttons">
                 <button className="vm edit" id="edit" onClick={() => this.toggleViewMode("edit")}>Edit Mode</button>
                 <button className="vm view" id="view" onClick={() => this.toggleViewMode("view")}>View Mode</button>
-                <button className='vm pdf' id="pdf"
-                onClick={this.downloadPDF}
-                disabled={!(isLoad===false)}>
-                    {isLoad ? (<span>Downloading</span>) : (<span>Download PDF</span>) }
-                </button>
             </div>
             <div className="yourResume">
                 <div className="cv-header">
